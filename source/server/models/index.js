32da4b47601b727bf949029bbee493c6c4384731
MikeSteiner <mike.steiner.77@gmail.com>
2016-11-29 19:43:27 +0200

    models init state, in test mode and probably not working :)

---
+++ source/server/models/index.js
@@ -0,0 +1,11 @@
+/**
+ * Created by admin on 28.11.2016 г..
+ */
+/* globals require module */
+'use strict';
+
+const Person = require('./person').Person,
+    User = require('./user').User,
+    ProfilePicture = require('./profile-picture').ProfilePicture;
+
+module.exports = { Person, User, ProfilePicture};
