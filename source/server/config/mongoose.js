992ba2dcdb558b215a4e30623e1a9f100fbd0eb4
MikeSteiner <mike.steiner.77@gmail.com>
2016-11-30 23:39:53 +0200

    updated with connect function call and connectionString variable parsed

--- source/server/config/mongoose.js
+++ source/server/config/mongoose.js
@@ -1,12 +1,19 @@
 /* globals require module */
 "use strict";
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-module.exports =  {};
+module.exports = function(connectionString) {
+    mongoose.Promise = global.Promise;
+    mongoose.connect(connectionString);
 
-// const mongoose = require("mongoose");
-//
-// module.exports = function(connectionString) {
-//     mongoose.connect(connectionString);
-// };
+    // var db = mongoose.connection;
+    // db.on('error', console.error.bind(console, 'connection error:'));
+    // db.once('open', function() {
+    //
+    //     console.log('We are now connected to connected to: ' + connectionString);
+    //
+    //
+    //     // all code
+    // });
+};
