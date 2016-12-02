3adc5f6e1f7e3f215cdf5c775a00ab4d268a26aa
MikeSteiner <mike.steiner.77@gmail.com>
2016-12-02 08:53:18 +0200

    test part commented

--- source/server/controllers/user-controller.js
+++ source/server/controllers/user-controller.js
@@ -3,67 +3,66 @@
 'use strict';
 
 const passport = require('passport');
-//---------------------------------------
-// AT test User.createAndSave start
-const mongoose = require("mongoose"),
-    config = require('../config');
-// require("../config/mongoose.js")(config.connectionString);
-require("../config/mongoose.js")(config.connectionStringForLocalTests);
-
-const dataForTest = require('../data');
-
-dataForTest.userCreateAndSave('Alex', 'Toplijski', 36, 'gender', 'persssi6', '123456', 'email@email.com', {src: 'source'})
-    .then((res) => {
-        // Create and Save user
-        console.log('------------- User -------------');
-        console.log(res);
-        return res;
-    })
-    .then((persssiUser) => {
-        // Create and Save event
-        var nowDt = new Date();
-        var res = dataForTest.eventCreateAndSave('First event title', persssiUser.userName, 'this si th body text, this si th body text, this si th body text, this si th body text', nowDt);
-
-        return res;
-    })
-    .then((dbEvent) => {
-        console.log('------------- Event -------------');
-        console.log(dbEvent);
-
-        // Add participant to event
-        dbEvent.participatingIn.push(dbEvent.get('author'));
-
-        return new Promise((resolve, reject) => {
-            dbEvent.save(function (error, res) {
-                if (error) {
-                    return reject(error);
-                }
-
-                return resolve(dbEvent);
-            });
-        });
-    })
-    .then((dbEvent) => {
-        // Create and add new comment to event
-        var nowDt = new Date();
-
-        var firstComment = dataForTest.commentCreate(dbEvent.author, 'comment comment.... by me', nowDt, false);
-
-        dbEvent.comments.push(firstComment);
-        dbEvent.save(function (error, res) {
-            if(error){
-                console.log(error);
-            }
-            console.log('------------- Event + one comment -------------');
-            console.log(res);
-            //return res;
-        });
-    })
-    .catch((err) => {
-        console.log(err);
-    });
-
-// AT test User.createAndSave end
+// //---------------------------------------
+// // AT test User.createAndSave star
+// const mongoose = require("mongoose");
+// const config = require('../config');
+// require("../config/mongoose.js")(config);
+//
+// const dataForTest = require('../data');
+//
+// dataForTest.userCreateAndSave('Alex', 'Toplijski', 36, 'gender', 'persssi', '123456', 'email@email.com', {src: 'source'})
+//     .then((res) => {
+//         // Create and Save user
+//         console.log('------------- User -------------');
+//         console.log(res);
+//         return res;
+//     })
+//     .then((persssiUser) => {
+//         // Create and Save event
+//         var nowDt = new Date();
+//         var res = dataForTest.eventCreateAndSave('First event title', persssiUser.userName, 'this si th body text, this si th body text, this si th body text, this si th body text', nowDt);
+//
+//         return res;
+//     })
+//     .then((dbEvent) => {
+//         console.log('------------- Event -------------');
+//         console.log(dbEvent);
+//
+//         // Add participant to event
+//         dbEvent.participatingIn.push(dbEvent.get('author'));
+//
+//         return new Promise((resolve, reject) => {
+//             dbEvent.save(function (error, res) {
+//                 if (error) {
+//                     return reject(error);
+//                 }
+//
+//                 return resolve(dbEvent);
+//             });
+//         });
+//     })
+//     .then((dbEvent) => {
+//         // Create and add new comment to event
+//         var nowDt = new Date();
+//
+//         var firstComment = dataForTest.commentCreate(dbEvent.author, 'comment comment.... by me', nowDt, false);
+//
+//         dbEvent.comments.push(firstComment);
+//         dbEvent.save(function (error, res) {
+//             if(error){
+//                 console.log(error);
+//             }
+//             console.log('------------- Event + one comment -------------');
+//             console.log(res);
+//             //return res;
+//         });
+//     })
+//     .catch((err) => {
+//         console.log(err);
+//     });
+//
+// // AT test User.createAndSave end
 
 
 module.exports = (data) => {
